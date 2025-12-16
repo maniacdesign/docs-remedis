85246bb968c0782dd5d1438f6c60942d0ac2541f
SIGIT TEGUH PRADANA <sigittp@gmail.com>
2025-12-16 16:11:15 +0700

    revisi

--- src/lib/layout.shared.tsx
+++ src/lib/layout.shared.tsx
@@ -5,22 +5,5 @@ export function baseOptions(): BaseLayoutProps {
     nav: {
       title: "Remedis Docs",
     },
-    links: [
-      {
-        text: "Docs Rawat Inap",
-        url: "/docsri",
-        active: "nested-url",
-      },
-      {
-        text: "Docs Rawat Jalan",
-        url: "/docsrj",
-        active: "nested-url",
-      },
-      {
-        text: "Docs Rawat Darurat",
-        url: "/docsigd",
-        active: "nested-url",
-      },
-    ],
   };
 }
